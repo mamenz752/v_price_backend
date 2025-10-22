4cb65aa5fbaa3e957dc241f0156cbf4892bb05f9
mamenz752 <mamenz752@gmail.com>
2025-10-22 15:45:18 +0900

    fix: delete ingest view

--- backend/code/ingest/urls.py
+++ backend/code/ingest/urls.py
@@ -3,11 +3,5 @@ from . import views
 
 app_name = 'ingest'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
     path('api/blobs/', views.BlobListView.as_view(), name='blob_list'),
-    
-    # インポート機能のURL
-    path('import/price/', views.ImportPriceView.as_view(), name='import_price'),
-    path('import/weather/', views.ImportWeatherView.as_view(), name='import_weather'),
-    path('import/all/', views.ImportAllDataView.as_view(), name='import_all'),
 ]

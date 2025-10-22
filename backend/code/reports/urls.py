b7ec70c112a4600fa9c299bd3d17ab702e3e972a
mamenz752 <mamenz752@gmail.com>
2025-10-22 14:53:20 +0900

    fix: /ingest/[vegetable_name] routing delete

--- backend/code/reports/urls.py
+++ backend/code/reports/urls.py
@@ -12,14 +12,4 @@ urlpatterns = [
     path('radish', views.RadishView.as_view(), name="radish"),
     path('potato', views.PotatoView.as_view(), name="potato"),
     path('onion', views.OnionView.as_view(), name="onion"),
-    
-    # 新しいIngestデータ表示用URLパス
-    path('ingest/cabbage', views.IngestCabbageView.as_view(), name="ingest_cabbage"),
-    path('ingest/chinese_cabbage', views.IngestChineseCabbageView.as_view(), name="ingest_chinese_cabbage"),
-    path('ingest/cucumber', views.IngestCucumberView.as_view(), name="ingest_cucumber"),
-    path('ingest/tomato', views.IngestTomatoView.as_view(), name="ingest_tomato"),
-    path('ingest/eggplant', views.IngestEggplantView.as_view(), name="ingest_eggplant"),
-    path('ingest/radish', views.IngestRadishView.as_view(), name="ingest_radish"),
-    path('ingest/potato', views.IngestPotatoView.as_view(), name="ingest_potato"),
-    path('ingest/onion', views.IngestOnionView.as_view(), name="ingest_onion"),
 ]

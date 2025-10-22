fb36dd09250e3643c1ab4813dc27e64f4269292a
mamenz752 <mamenz752@gmail.com>
2025-10-22 16:53:52 +0900

    wip: feedback ui

---
+++ backend/code/feedback/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+app_name = 'feedback'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name="index"),
+    path('chinese_cabbage', views.ChineseCabbageView.as_view(), name="chinese_cabbage"),
+    path('cabbage', views.CabbageView.as_view(), name="cabbage"),
+    path('cucumber', views.CucumberView.as_view(), name="cucumber"),
+    path('tomato', views.TomatoView.as_view(), name="tomato"),
+    path('eggplant', views.EggplantView.as_view(), name="eggplant"),
+    path('radish', views.RadishView.as_view(), name="radish"),
+    path('potato', views.PotatoView.as_view(), name="potato"),
+    path('onion', views.OnionView.as_view(), name="onion"),
+]
--- backend/code/feedback/views.py
+++ backend/code/feedback/views.py
@@ -1,3 +1,109 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class IndexView(TemplateView):
+    """
+    フィードバック機能のインデックスページを表示するビュー
+    """
+    template_name = 'feedback/feedback.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'フィードバック'
+        return context
+
+class ChineseCabbageView(TemplateView):
+    """
+    白菜のフィードバックページを表示するビュー
+    """
+    template_name = 'feedback/chinese_cabbage.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = '白菜フィードバック'
+        context['vegetable'] = '白菜'
+        return context
+
+class CabbageView(TemplateView):
+    """
+    キャベツのフィードバックページを表示するビュー
+    """
+    template_name = 'feedback/cabbage.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'キャベツフィードバック'
+        context['vegetable'] = 'キャベツ'
+        return context
+
+class CucumberView(TemplateView):
+    """
+    きゅうりのフィードバックページを表示するビュー
+    """
+    template_name = 'feedback/cucumber.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'きゅうりフィードバック'
+        context['vegetable'] = 'きゅうり'
+        return context
+
+class TomatoView(TemplateView):
+    """
+    トマトのフィードバックページを表示するビュー
+    """
+    template_name = 'feedback/tomato.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'トマトフィードバック'
+        context['vegetable'] = 'トマト'
+        return context
+
+class EggplantView(TemplateView):
+    """
+    なすのフィードバックページを表示するビュー
+    """
+    template_name = 'feedback/eggplant.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'なすフィードバック'
+        context['vegetable'] = 'なす'
+        return context
+
+class RadishView(TemplateView):
+    """
+    大根のフィードバックページを表示するビュー
+    """
+    template_name = 'feedback/radish.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = '大根フィードバック'
+        context['vegetable'] = '大根'
+        return context
+
+class PotatoView(TemplateView):
+    """
+    じゃがいものフィードバックページを表示するビュー
+    """
+    template_name = 'feedback/potato.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'じゃがいもフィードバック'
+        context['vegetable'] = 'じゃがいも'
+        return context
+
+class OnionView(TemplateView):
+    """
+    玉ねぎのフィードバックページを表示するビュー
+    """
+    template_name = 'feedback/onion.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = '玉ねぎフィードバック'
+        context['vegetable'] = '玉ねぎ'
+        return context

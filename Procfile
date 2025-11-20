8ba4a2ce29a9b9674084831d9c825c59554b6af1
mamenz752 <mamenz752@gmail.com>
2025-11-20 21:49:04 +0900

    update: show 5 vegetables

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: cd backend && python manage.py migrate
 web: cd backend && gunicorn config.wsgi:application --log-file -
--- backend/code/reports/urls.py
+++ backend/code/reports/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('chinese_cabbage', views.ChineseCabbageView.as_view(), name="chinese_cabbage"),
     path('cabbage', views.CabbageView.as_view(), name="cabbage"),
     path('cucumber', views.CucumberView.as_view(), name="cucumber"),
-    path('tomato', views.TomatoView.as_view(), name="tomato"),
+    # path('tomato', views.TomatoView.as_view(), name="tomato"),
     path('eggplant', views.EggplantView.as_view(), name="eggplant"),
     path('radish', views.RadishView.as_view(), name="radish"),
-    path('potato', views.PotatoView.as_view(), name="potato"),
-    path('onion', views.OnionView.as_view(), name="onion"),
+    # path('potato', views.PotatoView.as_view(), name="potato"),
+    # path('onion', views.OnionView.as_view(), name="onion"),
 ]
